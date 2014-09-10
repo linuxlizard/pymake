5a27eff581c049fc18732d95da94ffdaa014bf86
David Poole <testcluster@gmail.com>
2014-09-10 07:41:21 -0600

    add start of regression tests

--- sm.py
+++ sm.py
@@ -53,7 +53,7 @@ def parse_variable_ref(string):
     state = state_start
     token = ""
 
-    print( "state={0}".format(state))
+#    print( "state={0}".format(state))
 
     for c in string : 
 #        print("c={0} state={1}".format(c,state))
@@ -62,27 +62,27 @@ def parse_variable_ref(string):
                 state=state_dollar
         elif state==state_dollar:
             # looking for '(' or '$' or some char
-            if c=='(':
+            if c=='(' or c=='{':
+                opener = c
                 state = state_parsing
-                yield "$("
+                yield "$"+c
             elif c=='$':
                 # literal "$$"
                 yield "$$"
-                return None
+                return 
             elif not c in whitespace :
                 # single letter variable, e.g., $@ $x $_ etc.
                 token += c
                 yield "$"
                 yield token
-                return None
+                return 
         elif state==state_parsing:
-            if c==')':
+            if c==')' or c=='}':
                 yield token
 #                print("ending )")
-                yield ")"
-                return None
+                yield c 
+                return 
             elif c=='$':
-#                print("nested!")
                 # nested expression!  :-O
                 string.pushback()
                 # http://stackoverflow.com/questions/8407760/python-how-to-make-a-recursive-generator-function
@@ -91,7 +91,8 @@ def parse_variable_ref(string):
                         yield t
                 else :
                     # python 3.3 and above
-                    yield from parse_variable_ref(string)
+                    #yield from parse_variable_ref(string)
+                    pass
             else:
                 token += c
 
@@ -106,6 +107,9 @@ class ScannerIterator(object):
 
     def __iter__(self):
         return self
+
+    def next(self):
+        return self.__next__()
 
     def __next__(self):
         if self.idx >= self.max_idx:
@@ -138,30 +142,48 @@ def parse(infilename):
 #    for c in comment(s):
 #        print(c,end="")
 
+
 def main():
     import sys
     for infilename in sys.argv[1:]:
         parse(infilename)
+    test()
+
+def test():
 
     expression_tests = ( 
-        "$$", 
-        "$(CC)", 
-#        "${CC}", 
-        "$@", "$<", "$F", "$($($(FOO)))", "$($($F)))", 
-        "$(foo   )",
-        "$(info this is an info message)",
-        "$(error this is an error message)",
-        "$(findstring a,a b c)",
-        "$(patsubst %.c,%.o,x.c.c bar.c)",
-        "$(filter %.c %.s,$(sources))",
-        "$(objects:.o=.c)",
-        "$(filter-out $(mains),$(objects))",
-        "$(subst :, ,$(VPATH))",  # spaces are significant!
+        # string    result
+        ("$$",      ("$$",)),
+        ("$(CC)",   ("$(","CC",")")),
+        ("$( CC )", ("$("," CC ", ")")),
+        ("${CC}",   ("${","CC","}")),
+        ("$@",      ("$", "@",)),
+        ("$<",      ("$", "<",)),
+        ("$F",      ("$","F",)),
+        ("$($($(FOO)))",    ("$(","$(","$(","FOO",")","",")","",")")),
+        ("$($($(FOO)a)b)c", ("$(","$(","$(","FOO",")","a",")","b",")")),
+        ("$($($F)))",       ("$(","$(","$","F","",")","",")","",")")),
+        ("$($($Fqq)))",     ("$(","$(","$","F","qq",")","",")","",")")),
+        ("$(foo   )",       ("$(","foo   ",")")),
+        ("$(info this is an info message)",     ("$(","info this is an info message",")")),
+        ("$(error this is an error message)",   ("$(","error this is an error message",")")),
+        ("$(findstring a,a b c)",               ("$(","findstring a,a b c",")")),
+        ("$(patsubst %.c,%.o,x.c.c bar.c)",     ("$(","patsubst %.c,%.o,x.c.c bar.c",")")),
+        ("$(filter %.c %.s,$(sources))",        ("$(","filter %.c %.s,","$(","sources",")",")",)),
+#        "$(objects:.o=.c)",
+#        "$(filter-out $(mains),$(objects))",
+#        "$(subst :, ,$(VPATH))",  # spaces are significant!
     )
-    for s in expression_tests :
+    for test in expression_tests :
+        print(test)
+        s,result = test
         print("s={0}".format(s))
         my_iter = ScannerIterator(s)
-        print( "var={0}".format([ t for t in parse_variable_ref(my_iter)] ) )
+        tokens = [ t for t in parse_variable_ref(my_iter)] 
+        print( "tokens={0}".format(tokens) )
+
+        for v in zip(tokens,result):
+            assert  v[0]==v[1], v
 
     # this should fail
 #    print( "var={0}".format(parse_variable_ref(ScannerIterator("$(CC"))) )
