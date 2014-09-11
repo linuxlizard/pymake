f6f658e96736db6685afff9f5e08cb74b858e584
David Poole <davep@latches.marvell.com>
2014-09-11 10:24:20 -0600

    validate test makefile and parser test output

---
+++ validate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Validate output of test makefiles & my parser
+# davep 11-Sep-2014
+#
+# The output should be:  token space LHS token RHS
+#
+# The first character is the separator between the expected output and the
+# actual output. Usually "=" is used except in cases where the string itself
+# might contain "= .
+#
+# The separator token is followed by a single space (for human readability).
+# All spaces after the first space are significant! Don't strip() the strings!
+
+
+import sys
+
+# require Python 3.x for best Unicode handling
+if sys.version_info.major < 3:
+    raise Exception("Requires Python 3.x")
+
+def validate(infile):
+    while 1 :
+        # kill trailing \n
+        line = infile.readline()[:-1]
+        if len(line)<=0:
+            return
+
+        separator = line[0]
+        fields = line[2:].split(separator)
+        assert len(fields)==2,(len(fields),(line,fields))
+
+        for z in zip( (fields[0],), (fields[1],) ):
+            print( z )
+            msg = "\"{0}\"!=\"{1}\"".format(z[0],z[1])
+            if z[0]!=z[1] :
+                print("fail lhs=^",z[0],"$")
+                print("fail rhs=^",z[1],"$")
+            assert z[0]==z[1], (line,msg)
+
+def main(): 
+    for f in sys.argv[1:]:
+        if f =="-" :
+            validate(sys.stdin)
+        else:
+            with open(f) as infile:
+                validate(infile)
+
+if __name__=='__main__':
+    main()
+
