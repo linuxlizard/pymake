5f7f3294d1fc134e20ecfcb4523f617ffd64e5ea
David Poole <testcluster@gmail.com>
2014-09-13 07:23:08 -0600

    add Token() class; finish test cases with empty strings; correctly return string before tokenizing a sub-$() expression

--- sm.py
+++ sm.py
@@ -17,6 +17,19 @@ whitespace = set( ' \t\r\n' )
 class ParseError(Exception):
     pass
 
+# tinkering with a class for tokens
+#def Token(s): return s
+
+class Token(object):
+    def __init__(self,s):
+        self.string = s
+
+    def __str__(self):
+        return self.string
+
+    def __repr__(self):
+        return self.string
+
 def comment(string):
     state_start = 1
     state_eat_comment = 2
@@ -71,34 +84,42 @@ def parse_variable_ref(string):
             if c=='(' or c=='{':
                 opener = c
                 state = state_parsing
-                yield "$"+c
+                yield Token("$"+c)
             elif c=='$':
                 # literal "$$"
-                yield "$$"
+                yield Token("$$")
                 return 
             elif not c in whitespace :
                 # single letter variable, e.g., $@ $x $_ etc.
                 token += c
-                yield "$"
-                yield token
+                yield Token("$")
+                yield Token(token)
                 return 
         elif state==state_parsing:
             if c==')' or c=='}':
-                yield token
-#                print("ending )")
-                yield c 
+                # TODO make sure to match the open/close chars
+                yield Token(token)
+                yield Token(c)
                 return 
             elif c=='$':
                 # nested expression!  :-O
-                string.pushback()
-                # http://stackoverflow.com/questions/8407760/python-how-to-make-a-recursive-generator-function
-                if 1 : 
+                # if lone $$ token, preserve the $$ in the current token string
+                # otherwise, recurse into parsing a $() expression
+                if string.lookahead()=='$':
+                    token += "$$"
+                    string.next()
+                else:
+                    # return token so far
+                    yield Token(token)
+                    # restart token
+                    token = ""
+                    # push the '$' back onto the scanner
+                    string.pushback()
+                    # recurse into this scanner again
                     for t in parse_variable_ref(string):
                         yield t
-                else :
                     # python 3.3 and above
                     #yield from parse_variable_ref(string)
-                    pass
             else:
                 token += c
 
@@ -126,6 +147,7 @@ class ScannerIterator(object):
     def lookahead(self):
         if self.idx >= self.max_idx:
             raise StopIteration
+#        print("lookahead={0}".format(self.string[self.idx]))
         return self.string[self.idx]
 
     def pushback(self):
@@ -149,52 +171,71 @@ def parse(infilename):
 #        print(c,end="")
 
 
-def main():
-    import sys
-    for infilename in sys.argv[1:]:
-        parse(infilename)
-    test()
-
-def test():
+def expression_test():
 
     expression_tests = ( 
         # string    result
         ("$$",      ("$$",)),
+        ("$$$$$$",      ("$$$$$$",)),
         ("$(CC)",   ("$(","CC",")")),
         ("$( CC )", ("$("," CC ", ")")),
-        ("$(CC$$)",   ("$(","CC","$$", ")")),
-        ("$(CC$(LD))",   ("$(","CC","$(","LD",")",")")),
+        ("$(CC$$)",   ("$(","CC$$", ")")),
+        ("$(CC$(LD))",   ("$(","CC","$(","LD",")","",")")),
         ("${CC}",   ("${","CC","}")),
         ("$@",      ("$", "@",)),
         ("$<",      ("$", "<",)),
         ("$F",      ("$","F",)),
-        ("$($($(FOO)))",    ("$(","$(","$(","FOO",")","",")","",")")),
-        ("$($($(FOO)a)b)c", ("$(","$(","$(","FOO",")","a",")","b",")")),
-        ("$($($F)))",       ("$(","$(","$","F","",")","",")","",")")),
-        ("$($($Fqq)))",     ("$(","$(","$","F","qq",")","",")","",")")),
+        ("$Ff",      ("$","F","f",)),
+        ("$F$f",      ("$","F","$","f",)),
+        ("$$$F$f",      ("$$","$","F","$","f",)),
+        ("$($($(FOO)))",    ("$(","","$(","","$(","FOO",")","",")","",")")),
+        ("$($($(FOO)a)b)c", ("$(","","$(","","$(","FOO",")","a",")","b",")")),
+        ("$(a$(b$(FOO)a)b)c", ("$(","a","$(","b","$(","FOO",")","a",")","b",")")),
+        ("$($($F)))",       ("$(","","$(","","$","F","",")","",")","",")")),
+        ("$($($Fqq)))",     ("$(","","$(","","$","F","qq",")","",")","",")")),
         ("$(foo   )",       ("$(","foo   ",")")),
         ("$(info this is an info message)",     ("$(","info this is an info message",")")),
         ("$(error this is an error message)",   ("$(","error this is an error message",")")),
         ("$(findstring a,a b c)",               ("$(","findstring a,a b c",")")),
         ("$(patsubst %.c,%.o,x.c.c bar.c)",     ("$(","patsubst %.c,%.o,x.c.c bar.c",")")),
-        ("$(filter %.c %.s,$(sources))",        ("$(","filter %.c %.s,","$(","sources",")",")",)),
-#        "$(objects:.o=.c)",
-#        "$(filter-out $(mains),$(objects))",
-#        "$(subst :, ,$(VPATH))",  # spaces are significant!
+        ("$(filter %.c %.s,$(sources))",        ("$(",
+                                                    "filter %.c %.s,",
+                                                    "$(",
+                                                    "sources",
+                                                    ")",
+                                                    "",
+                                                    ")",
+                                                )),
+        ("$(objects:.o=.c)",        ("$(","objects:.o=.c",")",)),
+        ("$(filter-out $(mains),$(objects))",   ("$(","filter-out ","$(","mains",")",",","$(","objects",")","",")","",")")),
+        ("$(subst :, ,$(VPATH))",   ("$(","subst :, ,","$(","VPATH",")","",")")), # spaces are significant!
+#        ("$(foo)$(\#)bar=thisisanother\#testtesttest", ("$(","k
+        ("$(info = # foo#foo foo#foo foo#foo ###=# foo#foo foo#foo foo#foo ###)",
+          ("$(","info = # foo#foo foo#foo foo#foo ###=# foo#foo foo#foo foo#foo ###",")")),
     )
     for test in expression_tests :
-        print(test)
+        print("test={0}".format(test))
         s,result = test
         print("s={0}".format(s))
         my_iter = ScannerIterator(s)
         tokens = [ t for t in parse_variable_ref(my_iter)] 
-        print( "tokens={0}".format(tokens) )
+        print( "tokens={0}".format("||".join([t.string for t in tokens])) )
 
         for v in zip(tokens,result):
-            assert  v[0]==v[1], v
+#            assert  v[0]==v[1], v
+            assert  v[0].string==v[1], v
 
     # this should fail
 #    print( "var={0}".format(parse_variable_ref(ScannerIterator("$(CC"))) )
+
+def test():
+    expression_test()
+
+def main():
+    import sys
+    for infilename in sys.argv[1:]:
+        parse(infilename)
+    test()
 
 if __name__=='__main__':
     main()
