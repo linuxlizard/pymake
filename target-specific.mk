4ec2e1fb45ce0b79e60328cb4178138b5ff7ed79
David Poole <testcluster@gmail.com>
2014-09-19 21:19:03 -0600

    learning target specific rules

---
+++ target-specific.mk
@@ -0,0 +1,39 @@
+# Experiment with target specific rules.
+# GNU Make manual 6.11
+# davep 19-Sep-2014
+
+CC=gcc
+LD=ld
+
+all : specific normal
+
+normal : 
+	@echo $@ CC=$(CC) LD=$(LD)
+
+specific : CC=mycc
+specific : LD=myld
+specific : other
+	@echo $@ CC=$(CC) LD=$(LD)
+
+other : CC=$(shell which gcc)
+other : multiple
+	@echo $@ CC=$(CC) LD=$(LD)
+
+# multiple assignments 
+multiple: CC=intel-cc LD=intel-ld
+multiple: spaces
+	@echo $@ CC=$(CC) LD=$(LD)
+
+this is a test=CC
+spaces: $(this is a test)=this is a test
+spaces:
+	@echo $@ CC=$(CC) LD=$(LD) $(this is a test)
+
+# Anything after the assignment operator is treated as one token. The CC below creates one big string.
+FOO=BAR
+rules: CC=potato-cc LD=potato-ld spaces CROSS_COMPILE=potato $(FOO) potato
+rules:
+	@echo $@ CC=@@$(CC)@@ LD=@@$(LD)@@ CROSS_COMPILE=$(CROSS_COMPILE) TEST=$(this is a test)
+
+more-spaces : 
+
