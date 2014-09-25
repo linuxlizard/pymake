e1fbcb78e9581fcd0c56329cdb4c365665ce8483
David Poole <testcluster@gmail.com>
2014-09-24 21:37:04 -0600

    add weird whitespace tests

--- recipe.mk
+++ recipe.mk
@@ -52,10 +52,12 @@ where-do-i-end : ; @echo $@
     foofoofoo=999 # recipe is done here; leading spaces are ignored (eaten by recipe tokenizer?)
 $(info = 999=$(foofoofoo))    # leading spaces on name are ignored
 
+
 whitespace-then-tab : ; @echo $@
     
 # leading whitespace then tab is not valid error "missing separator"
 #    	@echo lots of leading whitespace here
+
 
 varrefs : ; @echo $@
 # the $(info) is error "commands commence before first target"
@@ -67,4 +69,54 @@ varrefs : ; @echo $@
 	@echo varref recipe date=$(shell date)
     # this $(error IS hit) (this comment is valid but Vim highlights as error)
 	@echo varref recipe info=$(error inside varref recipes)
+
+end-of-varrefs=yeah this ends varrefs rule for sure
+
+
+spaces-in-weird-places : ; @echo = spaces-in-weird-places=$@
+
+# the next line starts with a raw <tab>. Is valid. (starts empty shell? ignored?)
+	
+
+# the next line is <space><tab><EOL>. Is valid. (ignored)
+ 	
+
+# the next line starts with a raw <tab>. Is valid. (Sent to shell)
+	foo=bar printenv "foo"
+
+	@echo I am still inside $@
+
+# the next line is <tab><tab>. Is valid. The tabs are eaten.
+		@echo tab tab
+
+# the next line is <tab><space><tab>. Is valid. The tabs/spaces are eaten.
+	 	@echo tab space tab
+
+# the next line is <tab><space><tab><space><space><space>. Is valid. The tabs/spaces are eaten.
+	 	   @echo tab space tab space space space
+
+# the next line is has trailing <tab>s and <space>s; can see the whitespace on a hexdump of this output
+# the trailing whitespace is preserved
+	@echo I have trailing whitespacespaces                  	   	
+
+# this line ends the recipe; note can be interpretted as either an assignment or a shell recipe
+ 	foo=bar printenv "foo"
+$(info = bar printenv "foo"=$(foo))
+
+whitespace-error : ; @echo $@
+# the next line is <space><tab> then the echo
+# error "missing separator"
+# 	@echo space tab
+
+# error "missing separator"
+#foo bar baz
+
+ifdef foo
+endif
+
+# Need to avoid confusion between recipe trailing rule with ';' and the recipes
+# on the next line
+# This is not valid.
+#weird-semicolon:
+#; @echo $@
 
