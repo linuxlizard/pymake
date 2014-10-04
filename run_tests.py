f2e899b4ef0aae164297c5e187cb46ead2f534c8
David Poole <testcluster@gmail.com>
2014-10-04 10:54:18 -0600

    add a test to see what GNU Make will and won't accept for directives in context of assignment and rules

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,10 @@
 # davep 27-Sep-2014
 
 import sys
+import subprocess
+import tempfile
 
+import sm
 from sm import ScannerIterator
 
 # require Python 3.x 
@@ -44,6 +47,53 @@ def run_all_tests():
     statement_test.run()
     comment_test.run()
 
+def directives_test(): 
+    # Build makefiles with directives as rule and as assignment statement.
+    # Run make. List the ones that succeed, fail.
+
+    # GNU Make likely parses Makefiles looking for directives by tokenizing by
+    # the space. 
+    #   ifdef:      <--- allowed
+    #   ifdef :     <--- parse fail
+    #
+#    makefile_str = "{0}=foo\nall: ; @echo target=$@ $$$$\n"
+    makefile_str = "{0} = foo\nall: ; @echo target=$@ $$$$\n"
+#    makefile_str = "{0} : ; @echo target=$@ $$$$\n"
+#    makefile_str = "{0} : ; @echo target=$@ $$$$\n"
+
+    make = "/usr/bin/make"
+
+    failed = {}
+    passed = {}
+
+    for target in sm.directive : 
+
+        with tempfile.NamedTemporaryFile("w",prefix="./",delete=True) as outfile : 
+            outfile.write( makefile_str.format(target) );
+            outfile.flush()
+            
+#            print("target={0}".format(target))
+            
+            cmd = ( "/bin/cat", outfile.name )
+            output = subprocess.check_output( cmd, shell=False )
+#            print("{0}".format(output))
+
+            cmd = ( make, "-f", outfile.name )
+            try : 
+                output = subprocess.check_output( cmd, stderr=subprocess.STDOUT, shell=False )
+#                print("{0}".format(output))
+                passed[target] = output
+            except subprocess.CalledProcessError as err:
+                failed[ target ] = err.output
+#                print("err={0}".format(err))
+                pass
+
+    for f in failed : 
+        print("failed {0} {1}".format(f,failed[f]))
+    for s in passed:
+        print("passed {0} {1}".format(s,passed[s]))
+
 if __name__=='__main__':
+#    directives_test()
     run_all_tests()
 
