f3e68610fec7f958f52378ffd0ddf5db4b52f29d
David Poole <testcluster@gmail.com>
2014-11-11 07:34:50 -0700

    move ScannerIterator into own files (so can share between vline and pymake)

---
+++ scanner.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+
+# require Python 3.x because reasons
+if sys.version_info.major < 3:
+    raise Exception("Requires Python 3.x")
+
+class ScannerIterator(object):
+    # string iterator that allows look ahead and push back
+    # can also push/pop state (for deep lookaheads)
+    def __init__(self,data):
+        self.data = data
+        self.idx = 0
+        self.max_idx = len(self.data)
+        self.state_stack = []
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.__next__()
+
+    def __next__(self):
+        if self.idx >= self.max_idx:
+            raise StopIteration
+        self.idx += 1
+        return self.data[self.idx-1]
+
+    def lookahead(self):
+        if self.idx >= self.max_idx:
+            return None
+#        print("lookahead={0}".format(self.data[self.idx]))
+        return self.data[self.idx]
+
+    def pushback(self):
+        if self.idx <= 0 :
+            raise StopIteration
+        self.idx -= 1
+
+    def push_state(self):
+        self.state_stack.append(self.idx)
+        print( "push stack=", self.state_stack )
+
+    def pop_state(self):
+        print( "pop stack=", self.state_stack )
+        self.idx = self.state_stack.pop()
+
+    def remain(self):
+        # Test/debug method. Return what remains of the data.
+        return self.data[self.idx:]
+
+    def stop(self):
+        # truncate the iterator at the current position
+        assert self.idx < self.max_idx, self.idx
+
+        # kill anything after the current position
+        self.data = self.data[:self.idx]
+        self.max_idx = len(self.data)
+
