3c8e23e753ac2c137955f37da16b5639ac7b9d19
David Poole <testcluster@gmail.com>
2014-11-12 08:24:55 -0700

    clean up some tests; add a TEST_ME so can include the file in other makefiles

--- lispy.mk
+++ lispy.mk
@@ -6,15 +6,14 @@ define car
 $(firstword $(1))
 endef
 
-list=a b c d e f g 
-$(info car=$(call car,$(list)))
-
 define cdr
 $(wordlist 2,$(words $(1)),$(1))
 endef
 
+ifdef TEST_ME
 list=a b c d e f g 
-$(info $(wordlist 2,$(words $(list)),$(list)))
+$(info list=$(list))
+$(info car=$(call car,$(list)))
 $(info cdr=$(call cdr,$(list)))
 $(info cdr=$(call cdr,$(call cdr,$(list))))
 $(info cdr=$(call cdr,$(call cdr,$(call cdr,$(list)))))
@@ -26,4 +25,5 @@ $(info cdr=$(call cdr,$(call cdr,$(call cdr,$(call cdr,$(call cdr,$(call cdr,$(c
 $(info car=$(call car,$(call cdr,$(list))))
 
 lispy_all:;@:
+endif
 
