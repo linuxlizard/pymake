0b643146d854eefbe160afa1d3e671ecc1d552cb
David Poole <testcluster@gmail.com>
2014-11-19 17:09:27 -0700

    add additional export/unexport tests

--- export.mk
+++ export.mk
@@ -1,27 +1,38 @@
+a=b c d e f g
+
+export CC=gcc
+
+# export expression (note the backslash that makes my life diffcult)
 export\
 CC=gcc
 
+# bare export says "export everything by default"
 export
+
+unexport 
+export A B C D E F G
+unexport A B C D E F G
 
 CC=gcc
 LD=ld
 RM=rm
 export CC LD RM
+export $(CC) $(LD) $(RM)
 
 # multiple directives?
 # nope. Looks like only export only allows exporession on RHS
 export override LD=ld
-ifneq ("$(override LD)","ld")
-$(error export override error)
-endif
+#ifneq ("$(override LD)","ld")
+#$(error export override error)
+#endif
 
 # make 3.81 "export define" not allowed ("missing separator")
 # make 3.82 works
 # make 4.0  works
 # (See also error13.mk)
-define foo
-bar
-endef
+#define foo
+#bar
+#endef
 $(info $(call foo))
 
 export foo
