4de90f04a42336025190647b0fe15756c2500c74
David Poole <testcluster@gmail.com>
2023-01-25 06:39:02 -0700

    don't look for external files so we're more reliable

--- tests/words.mk
+++ tests/words.mk
@@ -1,6 +1,6 @@
 $(info $(words foo bar baz))
 $(info $(words      foo       bar 				baz			))
-$(info $(words $(shell ls *.mk)))
+$(info $(words a b c d e f g h i j k l m n o p q r s t u v w x y z))
 
 foo=
 $(info $(words $(foo)))
@@ -9,7 +9,8 @@ $(info $(words ))
 
 # "Returns the number of words in text. Thus, the last word of text is:"
 # -- gnu make manual
-text := $(shell ls *.py)
+text := A B D E F G H I J K L M N O P Q R S T U V W X Y Z _ ! @ $$ % ^ & * ( ) iamlast
+$(info $(words $(text)))
 $(info $(word $(words $(text)),$(text)))
 
 @:;@:
