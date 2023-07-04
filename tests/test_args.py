ff40d4ab2a2d84d2c66d83e5cc6856786796838f
David Poole <testcluster@gmail.com>
2023-07-04 16:02:13 -0600

    add a few more tests for pargs.py

--- tests/test_args.py
+++ tests/test_args.py
@@ -43,8 +43,45 @@ def test_parse_args_sexpr():
 def test_no_builtin_rules():
     args = pargs.parse_args(('-r', '-f', '/dev/null'))
     assert args.filename == "/dev/null"
+    assert args.no_builtin_rules
 
     args = pargs.parse_args(('--no-builtin-rules', '-f', '/dev/null'))
     assert args.filename == "/dev/null"
     assert args.no_builtin_rules
 
+def test_warn_undefined():
+    args = pargs.parse_args(('--warn-undefined-variables', '-f', '/dev/null'))
+    assert args.filename == "/dev/null"
+    assert args.warn_undefined_variables
+
+def test_dotfile():
+    args = pargs.parse_args(('--dotfile', 'makefile.dot', '-f', '/dev/null'))
+    assert args.filename == "/dev/null"
+    assert args.dotfile == 'makefile.dot'
+
+def test_html():
+    args = pargs.parse_args(('--html', 'makefile.html', '-f', '/dev/null'))
+    assert args.filename == "/dev/null"
+    assert args.htmlfile == 'makefile.html'
+
+def test_directory():
+    args = pargs.parse_args(('-C', 'build', '-f', '/dev/null'))
+    assert args.filename == "/dev/null"
+    assert len(args.directory)==1
+    assert args.directory[0] == 'build'
+
+    args = pargs.parse_args(('--directory', 'build', '-f', '/dev/null'))
+    assert args.filename == "/dev/null"
+    assert len(args.directory)==1
+    assert args.directory[0] == 'build'
+
+def test_multiple_directory():
+    args = pargs.parse_args(('-C', 'build', '-C', 'docs' ))
+    assert len(args.directory)==2
+    assert args.directory[0] == 'build'
+    assert args.directory[1] == 'docs'
+
+    args = pargs.parse_args(('--directory', 'build', '--directory', 'docs' ))
+    assert len(args.directory)==2
+    assert args.directory[0] == 'build'
+    assert args.directory[1] == 'docs'
